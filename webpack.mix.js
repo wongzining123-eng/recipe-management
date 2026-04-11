ed4700a293bc41da0bd5c28d14d35552c0a536d8
KaiX-Code <nookkaixiang0823@gmail.com>
2026-04-11 14:19:06 +0800

    Authentication Setup
    If need files from public directory (app.css, app.js, and mix-manifest.json), execute command "npm run dev"

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
+process.env.NODE_ENV = 'test';
+
 const mix = require('laravel-mix');
+const webpack = require('webpack');
 
 /*
  |--------------------------------------------------------------------------
@@ -6,12 +9,13 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
+
+mix.options({ progress: false });
